f7af27993f4bd029e7640d2729388aa0d1e5f5a6
Rafael de Oliveira Domenegueti <69098216+RafaelDomenegueti@users.noreply.github.com>
2021-01-04 01:31:46 -0300

    V1

---
+++ JavaScript/Script.js
@@ -0,0 +1,32 @@
+var data_hora = document.getElementById("data-hora")
+var corpo = document.getElementById("Corpo")
+
+// Data e Hora
+var now = new Date()
+
+var dia = now.getDate()
+var mes = now.getMonth()+1
+var ano = now.getFullYear()
+
+var hora = now.getHours()
+var minuto = now.getMinutes()
+
+setInterval(function(){
+    now = new Date()
+
+    dia = now.getDate()
+    mes = now.getMonth()+1
+    var ano = now.getFullYear()
+
+    hora = now.getHours()
+    if (hora <= 9){hora="0"+hora}
+    minuto = now.getMinutes()
+    if (minuto <= 9){minuto="0"+minuto}
+
+    data_hora.innerHTML = `${hora}:${minuto} &#x2022 ${dia}/${mes}/${ano}`
+}, 100)
+
+// Backgrounds
+var Background = Math.floor(Math.random() * 6 + 1)
+console.log(Background)
+corpo.style.backgroundImage = 'url(../Backgrounds/'+ Background +'.gif)'
