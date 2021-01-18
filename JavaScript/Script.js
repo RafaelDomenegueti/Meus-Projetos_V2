d0c94e5494446b2528fb4166013b34bfccdde833
Rafael de Oliveira Domenegueti <69098216+RafaelDomenegueti@users.noreply.github.com>
2021-01-18 14:24:29 -0300

    vx

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,6 +1,7 @@
 var data_hora = document.getElementById("data-hora")
 var corpo = document.getElementById("corpo")
 var menuapp = document.getElementById("menuapp")
+var sobre_janela = document.getElementById("sobre_janela")
 
 // Data e Hora
 var now = new Date()
@@ -42,3 +43,65 @@ function atividades(){
         console.log("Block")
     }
 }
+
+// Sobre
+function sobre(){
+    if (sobre_janela.style.display == "block"){
+        sobre_janela.style.display="none"
+        console.log("None")
+    }else {
+        sobre_janela.style.display="block"
+        console.log("Block")
+    }
+}
+
+// Janelas arrastaveis
+window.onload = function () {
+    Dragable(document.getElementById("sobre_janela"));
+};
+
+//Adiciona eventos pra navegadores modernos e antigos
+function addEvent(el, type, callback)
+{
+    if (el.addEventListener) {
+       el.addEventListener(type, callback);
+    } else if (el.attachEvent) {
+       el.attachEvent("on" + type, callback);
+    }
+}
+
+function Dragable(el)
+{
+    var isMove = false, //Verifica se esta se movendo
+        x = 0, y = 0, //Pega as coordenadas do mouse
+        xel = 0, yel = 0; // ultima posição do elemento
+
+    addEvent(el, "mousedown", function (e) {
+        isMove = true;
+
+        el.className += " isMoving";
+
+        x = window.event ? window.event.clientX : e.pageX;
+        y = window.event ? window.event.clientY : e.pageY;
+
+        xel = x - el.offsetLeft;
+        yel = y - el.offsetTop;
+    });
+
+    addEvent(document, "mousemove", function (e) {
+        if (isMove) {
+            e.preventDefault();
+
+            x = window.event ? window.event.clientX : e.pageX;
+            y = window.event ? window.event.clientY : e.pageY;
+
+            el.style.left = (x - xel) + 'px';
+            el.style.top  = (y - yel) + 'px';
+        }
+    });
+
+    addEvent(document, "mouseup", function () {
+        el.className = String(el.className).replace(/(^|\s)isMoving(\s|$)/g, " ");
+        isMove = false;
+    });
+};
