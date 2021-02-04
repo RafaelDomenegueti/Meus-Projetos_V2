6cc02388969baf1c66ce34e10871802d5c26d482
Rafael de Oliveira Domenegueti <69098216+RafaelDomenegueti@users.noreply.github.com>
2021-02-03 22:08:32 -0300

    vx

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,141 +1,151 @@
-var data_hora = document.getElementById("data-hora")
-var corpo = document.getElementById("corpo")
-var menuapp = document.getElementById("menuapp")
-var sobre_janela = document.getElementById("sobre_janela")
-var sobre_janela_topbar = document.getElementById("sobre_janela_topbar")
-var close_button = document.getElementById("close_button")
+// Variaveis de Captura 
+    var data_hora = document.getElementById("data-hora")
+    var corpo = document.getElementById("corpo")
+    var menuapp = document.getElementById("menuapp")
+    var sobre_janela = document.getElementById("sobre_janela")
+    var sobre_janela_topbar = document.getElementById("sobre_janela_topbar")
+    var close_button = document.getElementById("close_button")
+
 
 // Data e Hora
-var now = new Date()
 
-var dia = now.getDate()
-var mes = now.getMonth()+1
-var ano = now.getFullYear()
+    // Variaveis da Data e Hora
+        var now = new Date()
 
-var hora = now.getHours()
-var minuto = now.getMinutes()
+        var dia = now.getDate()
+        var mes = now.getMonth()+1
+        var ano = now.getFullYear()
 
-setInterval(function(){
-    now = new Date()
+        var hora = now.getHours()
+        var minuto = now.getMinutes()
 
-    dia = now.getDate()
-    mes = now.getMonth()+1
-    var ano = now.getFullYear()
+    setInterval(function(){
+        now = new Date()
 
-    hora = now.getHours()
-    if (hora <= 9){hora="0"+hora}
-    minuto = now.getMinutes()
-    if (minuto <= 9){minuto="0"+minuto}
+        dia = now.getDate()
+        mes = now.getMonth()+1
+        var ano = now.getFullYear()
 
-    data_hora.innerHTML = `${hora}:${minuto} &#x2022 ${dia}/${mes}/${ano}`
-}, 100)
+        hora = now.getHours()
+        if (hora <= 9){hora="0"+hora}
+        minuto = now.getMinutes()
+        if (minuto <= 9){minuto="0"+minuto}
 
-// Animações
-function backbuttonon(p) {
-    document.getElementById(p).style.backgroundColor="#6D6A75"
-}
-function backbuttonout(p) {
-    document.getElementById(p).style.backgroundColor="#1E1E24"
-}
-function appanimationon(a){
-    document.getElementById(a).style.width="250px"
-}
-function appanimationoff(a){
-    document.getElementById(a).style.width="150px"
-}
+        data_hora.innerHTML = `${hora}:${minuto} &#x2022 ${dia}/${mes}/${ano}`
+    }, 100)
+
+
+// Animações dos Botões
+    function backbuttonon(p) {
+        document.getElementById(p).style.backgroundColor="#6D6A75"
+    }
+    function backbuttonout(p) {
+        document.getElementById(p).style.backgroundColor="#1E1E24"
+    }
+    function appanimationon(a){
+        document.getElementById(a).style.width="250px"
+    }
+    function appanimationoff(a){
+        document.getElementById(a).style.width="150px"
+    }
+
 
 // Backgrounds
-var Background = Math.floor(Math.random() * 6 + 1)
-console.log(Background)
-corpo.style.backgroundImage = 'url(../Images/Backgrounds/'+ Background +'.gif)'
+    var Background = Math.floor(Math.random() * 6 + 1)
+    console.log(Background)
+    corpo.style.backgroundImage = 'url(../Images/Backgrounds/'+ Background +'.gif)'
+
 
 // Menu de Atividades
-function atividades(){
-    if (menuapp.style.display == "block"){
-        menuapp.style.display="none"
-        console.log("None")
-    }else {
-        menuapp.style.display="block"
-        console.log("Block")
+    function atividades(){
+        if (menuapp.style.display == "block"){
+            menuapp.style.display="none"
+            console.log("None")
+        }else {
+            menuapp.style.display="block"
+            console.log("Block")
+        }
     }
-}
+
 
 // Apps
-function game(){
-    menuapp.style.display="none"
-}
-function calculadora(){
-    menuapp.style.display="none"
-}
-function calendario(){
-    menuapp.style.display="none"
-}
+    function game(){
+        menuapp.style.display="none"
+    }
+    function calculadora(){
+        menuapp.style.display="none"
+    }
+    function calendario(){
+        menuapp.style.display="none"
+    }
+
 
 // Sobre
-function sobre(){
-    menuapp.style.display="none"
-    
-    if (sobre_janela.style.display == "block"){
-        sobre_janela.style.display="none"
-        console.log("None")
-    }else {
-        sobre_janela.style.display="block"
-        console.log("Block")
+    function sobre(){
+        menuapp.style.display="none"
+        
+        if (sobre_janela.style.display == "block"){
+            sobre_janela.style.display="none"
+            console.log("None")
+        }else {
+            sobre_janela.style.display="block"
+            console.log("Block")
+        }
     }
-}
+
 
 // Janelas arrastaveis
-window.onload = function () {
-    Dragable(sobre_janela_topbar);
-};
+    window.onload = function () {
+        Dragable(sobre_janela_topbar);
+    };
 
-//Adiciona eventos pra navegadores modernos e antigos
-function addEvent(el, type, callback)
-{
-    if (el.addEventListener) {
-       el.addEventListener(type, callback);
-    } else if (el.attachEvent) {
-       el.attachEvent("on" + type, callback);
-    }
-}
+    //Adiciona eventos pra navegadores modernos e antigos
+        function addEvent(el, type, callback)
+        {
+            if (el.addEventListener) {
+            el.addEventListener(type, callback);
+            } else if (el.attachEvent) {
+            el.attachEvent("on" + type, callback);
+            }
+        }
 
-function Dragable(el)
-{
-    var isMove = false, //Verifica se esta se movendo
-        x = 0, y = 0, //Pega as coordenadas do mouse
-        xel = 0, yel = 0; // ultima posição do elemento
+    function Dragable(el)
+    {
+        var isMove = false, //Verifica se esta se movendo
+            x = 0, y = 0, //Pega as coordenadas do mouse
+            xel = 0, yel = 0; // ultima posição do elemento
 
-    addEvent(el, "mousedown", function (e) {
-        isMove = true;
+        addEvent(el, "mousedown", function (e) {
+            isMove = true;
 
-        el.className += " isMoving";
-
-        x = window.event ? window.event.clientX : e.pageX;
-        y = window.event ? window.event.clientY : e.pageY;
-
-        xel = x - el.offsetLeft;
-        yel = y - el.offsetTop;
-    });
-
-    addEvent(document, "mousemove", function (e) {
-        if (isMove) {
-            e.preventDefault();
+            el.className += " isMoving";
 
             x = window.event ? window.event.clientX : e.pageX;
             y = window.event ? window.event.clientY : e.pageY;
 
-            el.style.left = (x - xel) + 'px';
-            el.style.top  = (y - yel) + 'px';
+            xel = x - el.offsetLeft;
+            yel = y - el.offsetTop;
+        });
 
-            // sobre_janela imita a sobre_janela_topbar
-            sobre_janela.style.left = (x - xel) + 'px';
-            sobre_janela.style.top  = (y - yel) + 'px';
-            sobre_janela.style.marginLeft = "0"
-        }
-    });
+        addEvent(document, "mousemove", function (e) {
+            if (isMove) {
+                e.preventDefault();
 
-    addEvent(document, "mouseup", function () {
-        el.className = String(el.className).replace(/(^|\s)isMoving(\s|$)/g, " ");
-        isMove = false;
-    });
-};
+                x = window.event ? window.event.clientX : e.pageX;
+                y = window.event ? window.event.clientY : e.pageY;
+
+                el.style.left = (x - xel) + 'px';
+                el.style.top  = (y - yel) + 'px';
+
+                // sobre_janela imita a sobre_janela_topbar
+                sobre_janela.style.left = (x - xel) + 'px';
+                sobre_janela.style.top  = (y - yel) + 'px';
+                sobre_janela.style.marginLeft = "0"
+            }
+        });
+
+        addEvent(document, "mouseup", function () {
+            el.className = String(el.className).replace(/(^|\s)isMoving(\s|$)/g, " ");
+            isMove = false;
+        });
+    };
